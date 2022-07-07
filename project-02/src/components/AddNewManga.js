6804d73277bd1bdda11be66bdcd6e868756107c4
HelloRave <huang.weiwei@trentglobal.edu.sg>
2022-07-07 03:53:25 +0000

    Refactored add new manga into functional component

---
+++ project-02/src/components/AddNewManga.js
@@ -0,0 +1,134 @@
+import React from 'react'
+
+export default function AddNewManga(props) {
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div>
+                    <label>Image</label>
+                    <input type="file" accept="image/*" />
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col-6'>
+                    <label className='form-label'>Title</label>
+                    <input type='text'
+                        className='form-control'
+                        name='title'
+                        value={props.title}
+                        onChange={props.updateFormField} />
+                </div>
+                <div className='col-6'>
+                    <label className='form-label'>Author</label>
+                    <input type='text'
+                        className='form-control'
+                        name='author'
+                        value={props.author}
+                        onChange={props.updateFormField} />
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col-6'>
+                    <label className='form-label'>Description</label>
+                    <textarea className='form-control'
+                        name='description'
+                        value={props.description}
+                        onChange={props.updateFormField} ></textarea>
+                </div>
+                <div className='col-6'>
+                    <p className='m-0'>Genre</p>
+
+                    {props.allGenre.map((obj) => {
+                        return (
+                            <React.Fragment key={obj.value}>
+                                <input type='checkbox'
+                                    className='form-check-input'
+                                    name='genre'
+                                    value={obj.value}
+                                    checked={props.genre.includes(obj.value)}
+                                    onChange={props.updateGenre} />
+                                <label className='form-check-label'>{obj.display}</label>
+                            </React.Fragment>
+
+                        )
+                    })}
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col-4'>
+                    <label className='form-label'>First-published</label>
+                    <input type='text'
+                        className='form-control'
+                        name='firstPublished'
+                        value={props.firstPublished}
+                        onChange={props.updateFormField} />
+                </div>
+                <div className='col-4'>
+                    <label className='form-label'>Volumes</label>
+                    <input type='number'
+                        className='form-control'
+                        name='volumes'
+                        value={props.volumes}
+                        onChange={props.updateNumberFormField} />
+                </div>
+                <div className='col-4'>
+                    <label className='form-label'>Chapters</label>
+                    <input type='number'
+                        className='form-control'
+                        name='chapters'
+                        value={props.chapters}
+                        onChange={props.updateNumberFormField} />
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col-4'>
+                    <label className='form-label'>Serialization</label>
+                    <input type='text'
+                        className='form-control'
+                        name='serialization'
+                        value={props.serialization}
+                        onChange={props.updateFormField} />
+                </div>
+                <div className='col-4'>
+                    <p>Ongoing</p>
+
+                    <input type='radio'
+                        name='ongoing'
+                        className='form-check-input'
+                        value='true'
+                        checked={props.ongoing === true}
+                        onChange={props.updateBooleanFormField} />
+                    <label className='form-check-label'>Yes</label>
+
+                    <input type='radio'
+                        name='ongoing'
+                        className='form-check-input'
+                        value='false'
+                        checked={props.ongoing === false}
+                        onChange={props.updateBooleanFormField} />
+                    <label className='form-check-label'>No</label>
+                </div>
+                <div className='col-4'>
+                    <p>Anime Adaptation</p>
+
+                    <input type='radio'
+                        name='animeAdaptation'
+                        className='form-check-input'
+                        value='true'
+                        checked={props.animeAdaptation === true}
+                        onChange={props.updateBooleanFormField} />
+                    <label>Yes</label>
+
+                    <input type='radio'
+                        name='animeAdaptation'
+                        className='form-check-input'
+                        value='false'
+                        checked={props.animeAdaptation === false}
+                        onChange={props.updateBooleanFormField} />
+                    <label>No</label>
+                </div>
+            </div>
+            <button className='btn btn-primary'>Add</button>
+        </div>
+    )
+}
--- project-02/src/components/LandingPage.js
+++ project-02/src/components/LandingPage.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Navbar, Nav, Container, Form, FormControl, Button } from 'react-bootstrap'
+import axios from 'axios'
+import AddNewManga from './AddNewManga'
 
 export default class LandingPage extends React.Component {
 
@@ -13,11 +15,19 @@ export default class LandingPage extends React.Component {
         chapters: '',
         serialization: '',
         ongoing: true,
-        animeAdaptation: true, 
+        animeAdaptation: true,
         plot: '',
         mainCharacters: '',
         supportingCharacters: '',
-        rating: ''
+        rating: '',
+        allGenre: []
+    }
+
+    async componentDidMount() {
+        let genreResponse = await axios.get('../json/genre.json')
+        this.setState({
+            allGenre: genreResponse.data
+        })
     }
 
     updateFormField = (event) => {
@@ -34,15 +44,15 @@ export default class LandingPage extends React.Component {
 
     updateBooleanFormField = (event) => {
         this.setState({
-            [event.target.name]: event.target.value === 'true' ? true : false 
+            [event.target.name]: event.target.value === 'true' ? true : false
         })
     }
 
     updateGenre = (event) => {
-        if (this.state.genre.includes(event.target.value)){
+        if (this.state.genre.includes(event.target.value)) {
             let index = this.state.genre.indexOf(event.target.value)
             this.setState({
-                genre: [...this.state.genre.slice(0,index), ...this.state.genre.slice(index + 1)]
+                genre: [...this.state.genre.slice(0, index), ...this.state.genre.slice(index + 1)]
             })
         } else {
             this.setState({
@@ -76,182 +86,51 @@ export default class LandingPage extends React.Component {
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
-                <div className='container'>
-                    <div className='row'>
-                        <div>
-                            <label>Image</label>
-                            <input type="file" accept="image/*" />
-                        </div>
-                    </div>
-                    <div className='row'>
-                        <div className='col-6'>
-                            <label className='form-label'>Title</label>
-                            <input type='text' 
-                                   className='form-control' 
-                                   name='title' 
-                                   value={this.state.title}
-                                   onChange={this.updateFormField}/>
-                        </div>
-                        <div className='col-6'>
-                            <label className='form-label'>Author</label>
-                            <input type='text' 
-                                   className='form-control'
-                                   name='author' 
-                                   value={this.state.author}
-                                   onChange={this.updateFormField} />
-                        </div>
-                    </div>
-                    <div className='row'>
-                        <div className='col-6'>
-                            <label className='form-label'>Description</label>
-                            <textarea className='form-control'
-                                      name='description' 
-                                      value={this.state.description}
-                                      onChange={this.updateFormField} ></textarea>
-                        </div>
-                        <div className='col-6'>
-                            <p className='m-0'>Genre</p>
-
-                            <input type='checkbox' 
-                                   className='form-check-input' 
-                                   name='genre'
-                                   value='action'
-                                   checked={this.state.genre.includes('action')}
-                                   onChange={this.updateGenre} />
-                            <label className='form-check-label'>Action</label>
-
-                            <input type='checkbox' 
-                                   className='form-check-input' 
-                                   name='genre'
-                                   value='adventure'
-                                   checked={this.state.genre.includes('adventure')}
-                                   onChange={this.updateGenre} />
-                            <label className='form-check-label'>Adventure</label>
-
-                            <input type='checkbox' className='form-check-input' name='' />
-                            <label className='form-check-label'>Comedy</label>
-
-                            <input type='checkbox' className='form-check-input' name='' />
-                            <label className='form-check-label'>Drama</label>
-
-                            <input type='checkbox' className='form-check-input' name='' />
-                            <label className='form-check-label'>Fantasy</label>
-                            
-                            <input type='checkbox' className='form-check-input' name='' />
-                            <label className='form-check-label'>Horror</label>
-                            
-                            <input type='checkbox' className='form-check-input' name='' />
-                            <label className='form-check-label'>Mystery</label>
-                            
-                            <input type='checkbox' className='form-check-input' name='' />
-                            <label className='form-check-label'>Sports</label>
-                        </div>
-                    </div>
-                    <div className='row'>
-                        <div className='col-4'>
-                            <label className='form-label'>First-published</label>
-                            <input type='text' 
-                                   className='form-control'
-                                   name='firstPublished' 
-                                   value={this.state.firstPublished}
-                                   onChange={this.updateFormField}/>
-                        </div>
-                        <div className='col-4'>
-                            <label className='form-label'>Volumes</label>
-                            <input type='number' 
-                                   className='form-control'
-                                   name='volumes' 
-                                   value={this.state.volumes}
-                                   onChange={this.updateNumberFormField}/>
-                        </div>
-                        <div className='col-4'>
-                            <label className='form-label'>Chapters</label>
-                            <input type='number' 
-                                   className='form-control'
-                                   name='chapters' 
-                                   value={this.state.chapters}
-                                   onChange={this.updateNumberFormField} />
-                        </div>
-                    </div>
-                    <div className='row'>
-                        <div className='col-4'>
-                            <label className='form-label'>Serialization</label>
-                            <input type='text' 
-                                   className='form-control'
-                                   name='serialization' 
-                                   value={this.state.serialization}
-                                   onChange={this.updateFormField} />
-                        </div>
-                        <div className='col-4'>
-                            <p>Ongoing</p>
-
-                            <input type='radio' 
-                                   name='ongoing' 
-                                   className='form-check-input'
-                                   value='true'
-                                   checked={this.state.ongoing === true}
-                                   onChange={this.updateBooleanFormField} />
-                            <label className='form-check-label'>Yes</label>
-
-                            <input type='radio' 
-                                   name='ongoing' 
-                                   className='form-check-input'
-                                   value='false'
-                                   checked={this.state.ongoing === false}
-                                   onChange={this.updateBooleanFormField} />
-                            <label className='form-check-label'>No</label>
-                        </div>
-                        <div className='col-4'>
-                            <p>Anime Adaptation</p>
-
-                            <input type='radio' 
-                                   name='animeAdaptation' 
-                                   className='form-check-input'
-                                   value='true'
-                                   checked={this.state.animeAdaptation === true}
-                                   onChange={this.updateBooleanFormField} />
-                            <label>Yes</label>
-
-                            <input type='radio' 
-                                   name='animeAdaptation' 
-                                   className='form-check-input' 
-                                   value='false'
-                                   checked={this.state.animeAdaptation === false}
-                                   onChange={this.updateBooleanFormField}/>
-                            <label>No</label>
-                        </div>
-                    </div>
-                </div>
+                <AddNewManga title={this.state.title}
+                             author={this.state.author}
+                             description={this.state.description}
+                             allGenre={this.state.allGenre}
+                             genre={this.state.genre}
+                             firstPublished={this.state.firstPublished}
+                             volumes={this.state.volumes}
+                             chapters={this.state.chapters}
+                             serialization={this.state.serialization}
+                             ongoing={this.state.ongoing}
+                             animeAdaptation={this.state.animeAdaptation}
+                             updateFormField={this.updateFormField}
+                             updateGenre={this.updateGenre}
+                             updateNumberFormField={this.updateNumberFormField}
+                             updateBooleanFormField={this.updateBooleanFormField}/>
                 <div className='container'>
                     <div className='row'>
                         <div className='col-12'>
                             <label className='form-label'>Plot</label>
                             <textarea className='form-control'
-                                      name='plot' 
-                                      value={this.state.plot}
-                                      onChange={this.updateFormField}></textarea>
+                                name='plot'
+                                value={this.state.plot}
+                                onChange={this.updateFormField}></textarea>
                         </div>
                         <div className='col-12'>
                             <label className='form-label'>Main Characters</label>
                             <textarea className='form-control'
-                                      name='mainCharacters' 
-                                      value={this.state.mainCharacters}
-                                      onChange={this.updateFormField}></textarea>
+                                name='mainCharacters'
+                                value={this.state.mainCharacters}
+                                onChange={this.updateFormField}></textarea>
                         </div>
                         <div className='col-12'>
                             <label className='form-label'>Supporting Characters</label>
                             <textarea className='form-control'
-                                      name='supportingCharacters' 
-                                      value={this.state.supportingCharacters}
-                                      onChange={this.updateFormField}></textarea>
+                                name='supportingCharacters'
+                                value={this.state.supportingCharacters}
+                                onChange={this.updateFormField}></textarea>
                         </div>
                         <div className='col-12'>
                             <label className='form-label'>Rating</label>
-                            <input type='number' 
-                                   className='form-control'
-                                   name='rating' 
-                                   value={this.state.rating}
-                                   onChange={this.updateNumberFormField}/>
+                            <input type='number'
+                                className='form-control'
+                                name='rating'
+                                value={this.state.rating}
+                                onChange={this.updateNumberFormField} />
                         </div>
                     </div>
                 </div>
