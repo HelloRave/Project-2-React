a1a0464db51db0bacd1040d9ffd3f8335c7455af
HelloRave <huang.weiwei@trentglobal.edu.sg>
2022-07-25 07:52:04 +0000

    Completed styling for landing page

--- project-02/src/App.js
+++ project-02/src/App.js
@@ -15,19 +15,18 @@ class App extends React.Component {
       return (
         <div className='landing d-flex justify-content-center align-items-center'>
           <video id="bgvid" src={require('./images/landing.mp4')} muted loop autoPlay></video>
-          <div className='text-div d-flex flex-column justify-content-center align-items-center'>
+          <div className='text-div d-flex flex-column justify-content-center align-items-center p-3'>
             <img className='logo' src={require('./images/logo.png')} />
-            <p>Some text here</p>
-            <button className='btn btn-primary'
+            <p className='landing-text text-center'>Discover more about the world of manga now!</p>
+            <button className='btn btn-primary btn-border'
                     onClick={() => {
                       this.setState({
                         active:'display'
                       })
                     }}>
-                      Explore!
+                      Explore
             </button>
           </div>
-
         </div>
       )
     }
